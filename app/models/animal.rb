144d631e1aa582f696e8e85b432a2c5b511e0bea
zjolliff <zjolliff@zjolliffs-MacBook-Pro-2.local>
2025-11-17 15:40:16 -0500

    add model validations for the req fields

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,17 @@
 class Animal < ApplicationRecord
+  # Validations
+  validates :name, presence: true
+  validates :species, presence: true
+  validates :breed, presence: true
+  validates :age, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :intake_date, presence: true
+  
+  # Set default value for adopted
+  after_initialize :set_defaults
+  
+  private
+  
+  def set_defaults
+    self.adopted ||= false if new_record?
+  end
 end
